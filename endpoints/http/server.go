26eb094fbb090ec16e5ea2b1d17b58af6d537042
zhangmj <zhangmengjin@qiyou.cn>
2025-02-12 21:24:40 +0800

    feat: 更新订单金额

--- endpoints/http/server.go
+++ endpoints/http/server.go
@@ -181,8 +181,13 @@ func registerRouter(g *gin.Engine) {
 	{
 		var orderController bill2.OrderController
 		billFe.POST("/order/create", orderController.CreateOrder)
-		billFe.POST("/order/notify", orderController.Notify)     // TODO:: 加密处理
-		billFe.GET("/order/pay-success", orderController.Return) // TODO:: 加密处理
+		billFe.POST("/order/notify", orderController.Notify) // TODO:: 加密处理
+	}
+
+	bill := g.Group("/bill")
+	{
+		var orderController bill2.OrderController
+		bill.GET("/order/pay-success", orderController.Return) // TODO:: 加密处理
 	}
 
 	// 内部调用: websocket on auth 回调.
