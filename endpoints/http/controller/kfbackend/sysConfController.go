331c34ca5a0285b652d586bd59366736817d8ef8
zhangmj <zhangmengjin@qiyou.cn>
2025-02-26 21:38:40 +0800

    feat: 更新kf设置

--- endpoints/http/controller/kfbackend/sysConfController.go
+++ endpoints/http/controller/kfbackend/sysConfController.go
@@ -16,17 +16,19 @@ type SysConfController struct {
 }
 
 type GetSysConfResponse struct {
-	CardID          string `json:"cardId" doc:"卡密id"`
-	Nickname        string `json:"nickname" doc:"昵称"`
-	AvatarURL       string `json:"avatarUrl" doc:"头像地址"`
-	WSFilter        bool   `json:"wsFilter" doc:"开启之后 检测ws行为"`
-	WechatFilter    bool   `json:"wechatFilter" doc:"非微信浏览器不能访问"`
-	AppleFilter     bool   `json:"appleFilter" doc:"苹果手机过滤器，开启后，只有苹果手机能访问"`
-	IPProxyFilter   bool   `json:"iPProxyFilter" doc:"代理ip过滤，开启后，代理ip不能访问"`
-	DeviceFilter    bool   `json:"DeviceFilter" doc:"设备异常过滤"`
-	SimulatorFilter bool   `json:"simulatorFilter" doc:"模拟器过滤，开启后，模拟器不能访问"`
-	Notice          string `json:"notice" doc:"滚动公告"`
-	NewMessageVoice bool   `json:"newMessageVoice" doc:"消息提示音"`
+	CardID           string `json:"cardId" doc:"卡密id"`
+	Nickname         string `json:"nickname" doc:"昵称"`
+	AvatarURL        string `json:"avatarUrl" doc:"头像地址"`
+	WSFilter         bool   `json:"wsFilter" doc:"开启之后 检测ws行为"`
+	WechatFilter     bool   `json:"wechatFilter" doc:"非微信浏览器不能访问"`
+	AppleFilter      bool   `json:"appleFilter" doc:"苹果手机过滤器，开启后，只有苹果手机能访问"`
+	IPProxyFilter    bool   `json:"iPProxyFilter" doc:"代理ip过滤，开启后，代理ip不能访问"`
+	DeviceFilter     bool   `json:"DeviceFilter" doc:"设备异常过滤"`
+	SimulatorFilter  bool   `json:"simulatorFilter" doc:"模拟器过滤，开启后，模拟器不能访问"`
+	Notice           string `json:"notice" doc:"滚动公告"`
+	NewMessageVoice  bool   `json:"newMessageVoice" doc:"消息提示音"`
+	QRCodeEnabled    bool   `json:"qrCodeEnabled" doc:"扫码开关"`
+	QRCodeScanFilter int    `json:"qrCodeScanFilter" doc:"扫码过滤: 1=关闭，2=过滤机房，3=过滤非大陆，4=过滤机房及非大陆"`
 }
 
 func (c *SysConfController) Get(ctx *gin.Context) {
@@ -42,33 +44,37 @@ func (c *SysConfController) Get(ctx *gin.Context) {
 	}
 
 	sysConf := GetSysConfResponse{
-		CardID:          setting.CardID,
-		Nickname:        setting.Nickname,
-		AvatarURL:       setting.AvatarURL,
-		WSFilter:        setting.WSFilter,
-		WechatFilter:    setting.WechatFilter,
-		AppleFilter:     setting.AppleFilter,
-		IPProxyFilter:   setting.IPProxyFilter,
-		DeviceFilter:    setting.DeviceFilter,
-		SimulatorFilter: setting.SimulatorFilter,
-		Notice:          setting.Notice,
-		NewMessageVoice: setting.NewMessageVoice,
+		CardID:           setting.CardID,
+		Nickname:         setting.Nickname,
+		AvatarURL:        setting.AvatarURL,
+		WSFilter:         setting.WSFilter,
+		WechatFilter:     setting.WechatFilter,
+		AppleFilter:      setting.AppleFilter,
+		IPProxyFilter:    setting.IPProxyFilter,
+		DeviceFilter:     setting.DeviceFilter,
+		SimulatorFilter:  setting.SimulatorFilter,
+		Notice:           setting.Notice,
+		NewMessageVoice:  setting.NewMessageVoice,
+		QRCodeEnabled:    setting.QRCodeEnabled,
+		QRCodeScanFilter: setting.QRCodeScanFilter,
 	}
 	c.Success(ctx, sysConf)
 
 }
 
 type PostSysConfRequest struct {
-	Nickname        string `json:"nickname" doc:"昵称"`
-	AvatarURL       string `json:"avatarUrl" doc:"头像地址"`
-	WSFilter        bool   `json:"wsFilter" doc:"开启之后 检测ws行为"`
-	WechatFilter    bool   `json:"wechatFilter" doc:"非微信浏览器不能访问"`
-	AppleFilter     bool   `json:"appleFilter" doc:"苹果手机过滤器，开启后，只有苹果手机能访问"`
-	IPProxyFilter   bool   `json:"iPProxyFilter" doc:"代理ip过滤，开启后，代理ip不能访问"`
-	DeviceFilter    bool   `json:"DeviceFilter" doc:"设备异常过滤"`
-	SimulatorFilter bool   `json:"simulatorFilter" doc:"模拟器过滤，开启后，模拟器不能访问"`
-	Notice          string `json:"notice" doc:"滚动公告"`
-	NewMessageVoice bool   `json:"newMessageVoice" doc:"消息提示音"`
+	Nickname         string `json:"nickname" doc:"昵称"`
+	AvatarURL        string `json:"avatarUrl" doc:"头像地址"`
+	WSFilter         bool   `json:"wsFilter" doc:"开启之后 检测ws行为"`
+	WechatFilter     bool   `json:"wechatFilter" doc:"非微信浏览器不能访问"`
+	AppleFilter      bool   `json:"appleFilter" doc:"苹果手机过滤器，开启后，只有苹果手机能访问"`
+	IPProxyFilter    bool   `json:"iPProxyFilter" doc:"代理ip过滤，开启后，代理ip不能访问"`
+	DeviceFilter     bool   `json:"deviceFilter" doc:"设备异常过滤"`
+	SimulatorFilter  bool   `json:"simulatorFilter" doc:"模拟器过滤，开启后，模拟器不能访问"`
+	Notice           string `json:"notice" doc:"滚动公告"`
+	NewMessageVoice  bool   `json:"newMessageVoice" doc:"消息提示音"`
+	QRCodeEnabled    bool   `json:"qrCodeEnabled" doc:"扫码开关"`
+	QRCodeScanFilter int    `json:"qrCodeScanFilter" doc:"扫码过滤: 1=关闭，2=过滤机房，3=过滤非大陆，4=过滤机房及非大陆"`
 }
 
 type PostSysConfResponse struct{}
@@ -102,6 +108,8 @@ func (c *SysConfController) Post(ctx *gin.Context) {
 	setting.Notice = req.Notice
 	setting.NewMessageVoice = req.NewMessageVoice
 	setting.CardID = cardID
+	setting.QRCodeEnabled = req.QRCodeEnabled
+	setting.QRCodeScanFilter = req.QRCodeScanFilter
 
 	err = kfsetting.SaveOne(reqCtx, setting)
 	if err != nil {
--- infrastructure/mysql/dao/kf_settings.go
+++ infrastructure/mysql/dao/kf_settings.go
@@ -7,17 +7,17 @@ type KFSettings struct {
 	CardID               string `json:"cardId" gorm:"column:card_id;unique;type:varchar(255)" doc:"卡密id"`
 	Nickname             string `json:"nickname" gorm:"column:nickname;type:varchar(255)"`
 	AvatarURL            string `json:"avatarUrl" gorm:"column:avatar_url;type:varchar(255)"`
-	WSFilter             bool   `json:"wsFilter" gorm:"column:ws_filter"`                           // 开启之后，检测ws行为.
-	WechatFilter         bool   `json:"wechatFilter" gorm:"column:wechat_filter"`                   // 非微信浏览器不能访问 .
-	AppleFilter          bool   `json:"appleFilter" gorm:"column:apple_filter"`                     // 苹果手机过滤器，开启后，只有苹果手机能访问,
-	IPProxyFilter        bool   `json:"iPProxyFilter" gorm:"column:ip_proxy_filter"`                // 代理ip过滤，开启后，代理ip不能访问
-	DeviceFilter         bool   `json:"DeviceFilter" gorm:"column:device_filter"`                   // 设备异常过滤.
-	SimulatorFilter      bool   `json:"simulatorFilter" gorm:"column:simulator_filter"`             // 模拟器过滤，开启后，模拟器不能访问
-	Notice               string `json:"notice" gorm:"column:notice;type:longtext"`                  // 滚动公告. maxLength 255
-	NewMessageVoice      bool   `json:"newMessageVoice" gorm:"column:new_message_voice"`            // 消息提示音.
-	QRCodeEnabled        bool   `json:"qrCodeEnabled" gorm:"column:qrcode_enabled"`                 // 二维码是否启用
-	QRCodeVersion        int    `json:"qrCodeVersion" gorm:"column:qrcode_version"`                 // 二维码版本 每次切换后更新
-	QRCodeEnabledNewUser bool   `json:"qrCodeEnabledNewUser" gorm:"column:qrcode_enabled_new_user"` // 启用停用新粉状态
+	WSFilter             bool   `json:"wsFilter" gorm:"column:ws_filter"`                               // 开启之后，检测ws行为.
+	WechatFilter         bool   `json:"wechatFilter" gorm:"column:wechat_filter"`                       // 非微信浏览器不能访问 .
+	AppleFilter          bool   `json:"appleFilter" gorm:"column:apple_filter"`                         // 苹果手机过滤器，开启后，只有苹果手机能访问,
+	IPProxyFilter        bool   `json:"iPProxyFilter" gorm:"column:ip_proxy_filter"`                    // 代理ip过滤，开启后，代理ip不能访问
+	DeviceFilter         bool   `json:"DeviceFilter" gorm:"column:device_filter"`                       // 设备异常过滤.
+	SimulatorFilter      bool   `json:"simulatorFilter" gorm:"column:simulator_filter"`                 // 模拟器过滤，开启后，模拟器不能访问
+	Notice               string `json:"notice" gorm:"column:notice;type:longtext"`                      // 滚动公告. maxLength 255
+	NewMessageVoice      bool   `json:"newMessageVoice" gorm:"column:new_message_voice"`                // 消息提示音.
+	QRCodeEnabled        bool   `json:"qrCodeEnabled" gorm:"column:qrcode_enabled"`                     // 二维码是否启用
+	QRCodeEnabledNewUser bool   `json:"qrCodeEnabledNewUser" gorm:"column:qrcode_enabled_new_user"`     // 启用停用新粉状态
+	QRCodeScanFilter     int    `json:"qrCodeScanFilter" gorm:"column:qrcode_scan_filter;default(1);" ` // doc:"扫码过滤: 1=关闭，2 =过滤机房，3=过滤非大陆，4 =过滤机房及非大陆"
 }
 
 func (KFSettings) TableName() string {
